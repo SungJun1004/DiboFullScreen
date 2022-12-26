cb9312e8f01d533962e18d9a8c78db9af83c6436
sunny <sunny@PARK_HOME>
2022-12-27 00:52:36 +0900

    초기 커밋.

---
+++ ExamCefSharp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using FullScreen;
+
+namespace DiboFullScreen
+{
+    public partial class Form1 : Form
+    {
+        private ChromiumWebBrowser _chrome = null;
+        Rectangle fullScrenn_bounds;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCefSharp("http://sap.com");
+        }
+
+        private void InitializeCefSharp(string StrURL)
+        {
+           
+            //쿠키 데이터 사용하는 방법
+            CefSettings settings = new CefSettings();
+            settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF";
+            Cef.Initialize(settings);
+           
+            //웹 사이트 이동
+             _chrome = new ChromiumWebBrowser(StrURL);
+
+            //한국어 설정
+            _chrome.BrowserSettings.AcceptLanguageList = "ko-KR";
+            //Main Form에 CefSharp컨트롤 추가
+            this.Controls.Add(_chrome);
+            //Main Form 전체 영역에 붙이기
+            _chrome.Dock = DockStyle.Fill;
+
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.FormBorderStyle = FormBorderStyle.None;
+            //this.Size = new Size(2000,1400);
+            //this.Location = new Point(0,0);
+            FormFullScreen();
+            InitializeURL();
+
+
+        }
+
+        private void FormFullScreen()
+        {
+            
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.Manual;
+
+            fullScrenn_bounds = Rectangle.Empty;
+
+            foreach (var screen in Screen.AllScreens)
+            {
+                fullScrenn_bounds = Rectangle.Union(fullScrenn_bounds, screen.Bounds);
+            }
+            this.Size = new Size(fullScrenn_bounds.Width, fullScrenn_bounds.Height);
+            this.Location = new Point(fullScrenn_bounds.Left, fullScrenn_bounds.Top);
+          //  MessageBox.Show(fullScrenn_bounds.Width.ToString()) ;
+        }
+        private void InitializeURL()
+        {
+            Form2 OpenCustURL = new Form2();
+            OpenCustURL.ShowDialog();
+
+            // 입력URL 호출
+            _chrome.LoadUrl(Form2.URLMessage);
+
+
+        }
+
+    }
+}
---
+++ ExamCefSharp/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FullScreen
+{
+
+        public partial class Form2 : Form
+    {
+        public static string URLMessage  = "";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            URLMessage = textBox1.Text;
+            Properties.Settings.Default.strURL = textBox1.Text;
+            Properties.Settings.Default.Save();
+            this.Close(); 
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // this.Size = new Size(2000, 1400);
+            // this.Location = new Point(1000,2000);
+            Rectangle fullScrenn_bounds; 
+            fullScrenn_bounds = Rectangle.Empty;
+
+            foreach (var screen in Screen.AllScreens)
+            {
+                fullScrenn_bounds = Rectangle.Union(fullScrenn_bounds, screen.Bounds);
+            }
+            this.Location = new Point(fullScrenn_bounds.Width/2 - 300, fullScrenn_bounds.Height/2 - 100);
+
+            textBox1.Text = Properties.Settings.Default.strURL;
+        }
+    }
+}
